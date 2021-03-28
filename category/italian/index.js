91e9c1d4fe723f0a59ec73a70f31f3b1c58b57e0
Angga Prasetya W <angga.pw@agiletechnica.com>
2021-03-29 02:30:03 +0800

    fix logic detail

--- category/italian/index.js
+++ category/italian/index.js
@@ -14,17 +14,20 @@
             let contentData = data.results.forEach(element => { 
                 content += 
                 `<div class="col-md-3">
-                        <div class="card" onClick="detail(event, ${element.id})">
-                            <a href="/pages/details/index.html">
-                                <img src="${element.image}" class="card-img-top" height="250px">
+                        <div class="card">
+                            <a href="">
+                               <div onClick="detail(event, ${element.id})">
+                                    <img src="${element.image}" class="card-img-top" height="250px">
+                               </div>
                             </a>
-                                <div class="card-body">
-                                    <a href="/pages/details/index.html">
-                                        <h6 class="card-title">${element.title}</h6>
-                                    </a>
-                                    <div class ="submitBtn">
-                                        <input class="btn btn-primary" type="submit" value="Wishlist" id="wishlistButton">
-                                    </div>
+                            <div class="card-body">
+                                <a href="">
+                                    <h6 class="card-title" onClick="detail(event, ${element.id})">${element.title}</h6>
+                                </a>
+
+                                <div class ="submitBtn">
+                                    <input class="btn btn-primary" type="submit" value="Wishlist" id="wishlistButton">                                   
+                                </div>
                             </div>
                         </div>
                 </div>`
--- category/korean/index.js
+++ category/korean/index.js
@@ -1,4 +1,3 @@
-// window.onload = function() {
     const apiKey = ["80e73835c8934c0a8f67600904977073", "83f2261f53d549ad8d8451ba2ebde399", "10baa3c3fcac490ab89f070dd6a19ba9"]
     const random = Math.floor(Math.random() * apiKey.length)
     const randomAPIKey = apiKey[random]
@@ -15,18 +14,19 @@
                 content += 
                 `<div class="col-md-3">
                         <div class="card">
-                        <div onClick="detail(event, ${element.id})">
-                            <a href="/pages/details/index.html">
-                                <img src="${element.image}" class="card-img-top" height="250px">
+                            <a href="">
+                               <div onClick="detail(event, ${element.id})">
+                                    <img src="${element.image}" class="card-img-top" height="250px">
+                               </div>
                             </a>
                             <div class="card-body">
-                                <a href="/pages/details/index.html">
-                                    <h6 class="card-title">${element.title}</h6>
+                                <a href="">
+                                    <h6 class="card-title" onClick="detail(event, ${element.id})">${element.title}</h6>
                                 </a>
-                            </div>
-                                    <div class ="submitBtn">
-                                        <input class="btn btn-primary" type="submit" value="Wishlist" id="wishlistButton">                                   
-                                    </div>
+
+                                <div class ="submitBtn">
+                                    <input class="btn btn-primary" type="submit" value="Wishlist" id="wishlistButton">                                   
+                                </div>
                             </div>
                         </div>
                 </div>`
@@ -40,11 +40,9 @@
 
     let url = location.href;
 
-    const detail = (event,id) => {
+    const detail = (event, id) => {
         event.preventDefault()
         localStorage.setItem('dishID', id)
         localStorage.setItem("url",url)
         window.location.replace('/pages/details/index.html')
     }
-// }
-
