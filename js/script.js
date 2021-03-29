463c7d712209337f47bbb0fdecc551b7683429b6
efzedel16 <efzedel16@outlook.com>
2021-03-29 15:01:18 +0700

    Responsive

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
     const apiKey = ["80e73835c8934c0a8f67600904977073", "83f2261f53d549ad8d8451ba2ebde399", "10baa3c3fcac490ab89f070dd6a19ba9"]
     const random = Math.floor(Math.random() * apiKey.length)
     const randomAPIKey = apiKey[random]
-
     const endPoint = `https://api.spoonacular.com/recipes/random?number=5&apiKey=${randomAPIKey}`
     const recommendation = document.getElementById("recommendation");
+
     const fetchAPI = _ => {
         fetch(endPoint).then(response => response.json()).then(data => {
             let content = "";
